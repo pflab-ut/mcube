3d2d06fdc9ae00253d8a70aea3baa31071fa5402
Hiroyuki Chishiro <h.chishiro@gmail.com>
2018-10-03 14:46:46 +0900

    update docker

--- scripts/docker/docker.sh
+++ scripts/docker/docker.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-args=($@)
-#echo "args = " ${args[@]}
-
-# remove the first argument "docker".
-if [ $# -gt 1 ] ; then
-    args=(${args[@]:1})
-#    echo "args = " ${args[@]}
-fi
-
 uid=$(id -u)
 gid=$(id -g)
 code=$(realpath .)
@@ -17,4 +8,4 @@ docker run --rm -it \
        -v ${code}:/home/mcube \
        -e HOME=/home/mcube \
        -u ${uid}:${gid} \
-       chishiro/mcube-build /bin/bash -c "cd /home/mcube && make ${args} -j$(nproc)"
+       chishiro/mcube-build /bin/bash -c "cd /home/mcube && make -j$(nproc)"
