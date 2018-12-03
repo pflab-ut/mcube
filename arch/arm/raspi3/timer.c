5fc345323576f41977e272bbbfdcf203ec39e913
Hiroyuki Chishiro <h.chishiro@gmail.com>
2018-12-03 18:18:50 +0900

    update timer in raspi3

--- arch/arm/raspi3/timer.c
+++ arch/arm/raspi3/timer.c
@@ -10,22 +10,16 @@ void init_timer(unsigned long tick_us)
 {
   unsigned long cpu = get_cpu_id();
   timer_cntfrq = get_cntfrq_el0();
-  //  printk("get_timer_frequency() = %lu\n", timer_cntfrq);
     
   timer_tick = (timer_cntfrq * TICK_USEC) / (1000 * 1000);
-  //  printk("timer_tick = %lu\n", timer_tick);
-  /*
-    printk("timer_local_timer_ctrl_status = %x\n",
-    mmio_in32(LP_LOCAL_TIMER_CTRL_STATUS));
-  */
   /* clear interrupt flag and reload local timer */
   mmio_out32(LP_LOCAL_TIMER_WRITE_FLAGS,
              LP_LOCAL_TIMER_WRITE_FLAGS_INTERRUPT_FLAG_CLEAR
              | LP_LOCAL_TIMER_WRITE_FLAGS_LOCAL_TIMER_RELOADED);
   /* IRQ routing to core [cpu] */
   mmio_out32(LP_GPU_INTERRUPT_ROUTING,
-             /*LP_GPU_INTERRUPT_ROUTING_FIQ_CORE(cpu)
-               | */LP_GPU_INTERRUPT_ROUTING_IRQ_CORE(cpu));
+             LP_GPU_INTERRUPT_ROUTING_FIQ_CORE(cpu)
+             | LP_GPU_INTERRUPT_ROUTING_IRQ_CORE(cpu));
   /* route cor0cntv to core [cpu] irq */
   mmio_out32(LP_CORE_TIMERS_INTERRUPT_CTRL(cpu),
              LP_CORE_TIMERS_INTERRUPT_CTRL_CNTVIRQ_IRQ_CTRL);
