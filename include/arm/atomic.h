f8df8c758969eba650615e67cd0c96caa356111b
Hiroyuki Chishiro <h.chishiro@gmail.com>
2018-12-22 21:49:27 +0900

    update atomic_inc/dec() in arm

--- include/arm/atomic.h
+++ include/arm/atomic.h
@@ -12,46 +12,40 @@
 
 #define LOCK_PREFIX LOCK_PREFIX_HERE
 
-static inline void atomic_inc(atomic_t *v)
+static inline void cmpxchg(volatile unsigned long *ptr, unsigned long new, unsigned long old)
 {
-  unsigned long tmp, old, new;
-  old = v->counter;
-  new = v->counter + 1;
+  unsigned long tmp;
 #if 1
   unsigned long oldval;
   //  asm volatile("prfm    pstl1strm, %0" :: "r"(&v->counter));
-  asm volatile("1: ldaxr %w0, [%1]" : "=&r"(oldval) : "r"(&v->counter));
+  asm volatile("1: ldaxr %w0, [%1]" : "=&r"(oldval) : "r"(ptr));
   asm volatile("eor %0, %1, %2" : "=r"(tmp) : "r"(oldval), "r"(old));
   asm volatile("cbnz %0, 2f" :: "r"(tmp));
-  asm volatile("stxr %w0, %w2, [%1]" : "=&r"(tmp) : "r"(&v->counter), "r"(new));
+  asm volatile("stxr %w0, %w2, [%1]" : "=&r"(tmp) : "r"(ptr), "r"(new));
   asm volatile("cbnz %0, 1b" :: "r"(tmp));
   asm volatile("2:");
 #else  
   /* Large System Extension (LSE) in ARMv8.1 */
   asm volatile("mov %w0, %w1" : "=r"(tmp) : "r"(old));
-  asm volatile("cas %w0, %w1, [%2]" : "=r"(tmp), "=r"(new), "=r"(v->counter));
+  asm volatile("cas %w0, %w1, [%2]" : "=r"(tmp), "=r"(new), "=r"(*ptr));
 #endif
+}
+
+
+static inline void atomic_inc(atomic_t *v)
+{
+  unsigned long new, old;
+  new = v->counter + 1;
+  old = v->counter;
+  cmpxchg((unsigned long *) &v->counter, new, old);
 }
 
 static inline void atomic_dec(atomic_t *v)
 {
-  unsigned long tmp, old, new;
-  old = v->counter;
+  unsigned long new, old;
   new = v->counter - 1;
-#if 1
-  unsigned long oldval;
-  //  asm volatile("prfm    pstl1strm, %0" :: "r"(&v->counter));
-  asm volatile("1: ldaxr %w0, [%1]" : "=&r"(oldval) : "r"(&v->counter));
-  asm volatile("eor %0, %1, %2" : "=r"(tmp) : "r"(oldval), "r"(old));
-  asm volatile("cbnz %0, 2f" :: "r"(tmp));
-  asm volatile("stxr %w0, %w2, [%1]" : "=&r"(tmp) : "r"(&v->counter), "r"(new));
-  asm volatile("cbnz %0, 1b" :: "r"(tmp));
-  asm volatile("2:");
-#else  
-  /* Large System Extension (LSE) in ARMv8.1 */
-  asm volatile("mov %w0, %w1" : "=r"(tmp) : "r"(old));
-  asm volatile("cas %w0, %w1, [%2]" : "=r"(tmp), "=r"(new), "=r"(v->counter));
-#endif
+  old = v->counter;
+  cmpxchg((unsigned long *) &v->counter, new, old);
 }
 
 static inline int fetch_and_inc(int *i)
