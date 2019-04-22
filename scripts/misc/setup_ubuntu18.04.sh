53036451592adad37f7adf630afe66b18c3267e6
Hiroyuki Chishiro <h.chishiro@gmail.com>
2019-04-22 19:50:56 +0900

    update setup_ubuntu18.04.sh

--- scripts/misc/setup_ubuntu18.04.sh
+++ scripts/misc/setup_ubuntu18.04.sh
@@ -31,5 +31,5 @@ $prefix qemu qemu-system
 
 
 echo "\n"
-echo "If you use Qemu 2.12.0 (or later) for Raspberry Pi3 (ARM64), please see doc/user.tex"
-echo "If you use AXIS ISA for DOM, please use https://github.com/pflab-ut/llvm"
+echo "If you use QEMU 2.12.0 (or later) for Raspberry Pi3 (ARM64), please use https://www.qemu.org/download/."
+echo "If you use AXIS ISA for DOM, please use https://github.com/pflab-ut/llvm."
